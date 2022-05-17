470c72db79a040ba6098451c060dc33527196f78
luxianquan <18814663230@163.com>
2022-05-17 13:35:14 +0800

    DemoCommand

--- webhook/src/main/java/com/lcoil/web/controller/CallBackController.java
+++ webhook/src/main/java/com/lcoil/web/controller/CallBackController.java
@@ -1,9 +1,12 @@
 package com.lcoil.web.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.lcoil.commons.command.DemoCommand;
+import com.lcoil.commons.commandbus.CommandBus;
 import com.lcoil.commons.utils.ResponseResult;
 import com.lcoil.params.git.GitEventParams;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -17,10 +20,19 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class CallBackController {
 
+    @Autowired
+    private CommandBus commandBus;
+
     @PostMapping("/callback")
     @ResponseBody
     public ResponseResult callback(@RequestBody GitEventParams gitEventParams){
         return new ResponseResult(200, "成功", JSONObject.toJSONString(gitEventParams));
+    }
+
+    @PostMapping("/demoCommand")
+    public String check(@RequestBody DemoCommand demoCommand){
+        commandBus.send(demoCommand);
+        return "ok";
     }
 
 }
